1cffeb6b1b0aa48370aef94dc31afb8001b83352
inscode <dongdong11019@gmail.com>
2017-02-20 00:51:39 +0800

    pass

---
+++ encode.php
@@ -0,0 +1,32 @@
+<?php 
+
+function encrypt($str, $operation, $key) {
+    //加密uid
+    $key  = ord($key)+231;
+    if ($operation == "E") {
+        $len  = strlen($str);
+        $rest = "";
+        for ($i=0; $i < $len; $i++) { 
+            $rest .=$key+ord($str{$i});
+        }
+    }
+    //解密uid
+    if ($operation == "D") {
+        $arr = str_split($str,3);
+        $len = count($arr);
+        $rest= "";
+        for ($i=0; $i < $len; $i++) { 
+            $rest .= chr($arr[$i]-$key);
+        }
+    }
+    return $rest;
+}
+
+    $base   = "68feeb0d-a59d-763c-c0ef-ecad3b2dd3d9";
+    $encode = encrypt($base, "E", "lisp");
+    $decode = encrypt($encode, "D", 'lisp');
+    echo '原来---'.$base."<br/>";
+    echo "加密---".$encode."<br/>";
+    echo "解密---".$decode;
+
+ ?>
